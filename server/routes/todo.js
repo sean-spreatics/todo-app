7632a95a5ea61ec44ec2cdfba3764d95222deae9
sean-spreatics <sean@spreatics.com>
2022-12-12 12:24:57 +0900

    feat: Todo 수정 및 삭제 기능 구현

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -30,4 +30,62 @@ router.post('/todo', async (req, res) => {
   }
 });
 
+// PATCH localhost:PORT/todo/:todoId - edit a specific todo (UPDATE)
+// 수정 성공시; true -> res.send(true)
+// 수정 실패시; false -> res.send(false)
+router.patch('/todo/:todoId', async (req, res) => {
+  // console.log(req.body); // { title: 'my todo - 수정', done: true }
+  // console.log(req.params); // { todoId: '1' }
+  try {
+    let [isUpdated] = await Todo.update(
+      {
+        title: req.body.title,
+        done: req.body.done,
+      },
+      {
+        where: {
+          id: req.params.todoId,
+        },
+      }
+    );
+    // console.log(isUpdated);
+    // 수정 성공시; [ 1 ] -> 1
+    // 수정 실패시; [ 0 ] -> 0
+
+    // 수정 실패 (!0)
+    if (!isUpdated) {
+      return res.send(false);
+    }
+
+    // 수정 성공
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// DELETE localhost:PORT/todo/:todoId - remove a specific todo (DELETE)
+router.delete('/todo/:todoId', async (req, res) => {
+  // console.log(req.params); // { todoId: '8' }
+
+  try {
+    let isDeleted = await Todo.destroy({
+      where: {
+        id: req.params.todoId,
+      },
+    });
+    // console.log(isDeleted); // 1 or 0
+
+    // 삭제 실패 (!0)
+    if (!isDeleted) {
+      return res.send(false);
+    }
+
+    // 삭제 성공
+    res.send(true);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 module.exports = router;
